520141ca652c1f8dff9c8ac315f7cc8660e99282
Hieu Nguyen <hieund898@gmail.com>
2024-07-07 16:45:07 +0000

    feat: done

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config()
+
+const multer = require('multer');
+// SET STORAGE
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const { file } = req;
 
-
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
